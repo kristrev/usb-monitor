a0e64515eda8b1bb676f913ce85a95f4c50a5bc4
Kristian Evensen <kristian.evensen@gmail.com>
2018-12-23 15:43:18 +0100

    lanner: Ensure that port mask is wiped
    Ensure that port mask is wiped when we are done with mcu.

--- src/lanner_handler.h
+++ src/lanner_handler.h
@@ -49,6 +49,7 @@ struct lanner_port {
     struct lanner_shared *shared_info;
     uint8_t bitmask;
     uint8_t cur_cmd;
+    uint8_t rest_sub_cmd;
     //Lanner does not allow control of a single port, instead we must write
     //the complete bitmask every time
     uint8_t cur_state;
